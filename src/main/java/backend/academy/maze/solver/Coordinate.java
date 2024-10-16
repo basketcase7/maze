fa5be768ca49d9cc80c03508eb85fb73294a608e
Maxim <mac28@tpu.ru>
2024-10-16 13:58:24 +0700

    feat(record): Добавлен рекорд Coordinate
    Добавлен рекорд Coordinate для хранения информации о пути при решении лабиринта.

---
+++ src/main/java/backend/academy/maze/solver/Coordinate.java
@@ -0,0 +1,9 @@
+package backend.academy.maze.solver;
+
+/**
+ * Рекорд для сущности координаты
+ * @param row Строчка координаты
+ * @param col Столбец координаты
+ */
+public record Coordinate(int row, int col) {
+}
