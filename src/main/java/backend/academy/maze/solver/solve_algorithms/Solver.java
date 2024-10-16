aa61234f2a117ce6b1ddb042eefb5451b881d694
Maxim <mac28@tpu.ru>
2024-10-16 14:16:45 +0700

    feat(algorithm): Добавлен интерфейс Solver
    Добавлен интерфейс Solver, который будет реализован алгоритмами решения лабиринтов.

---
+++ src/main/java/backend/academy/maze/solver/solve_algorithms/Solver.java
@@ -0,0 +1,12 @@
+package backend.academy.maze.solver.solve_algorithms;
+
+import backend.academy.maze.generator.Cell;
+import backend.academy.maze.solver.Coordinate;
+import java.util.List;
+
+/**
+ * Интерфейс, который реализуется алгоритмами решения лабиринта
+ */
+public interface Solver {
+    List<Coordinate> solve(Cell[][] maze, Coordinate start, Coordinate end, int height, int width);
+}
